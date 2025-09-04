d88b7ef086358a82491da704650e14a6c0977581
Daniel-Collado <danielcollado82@gmail.com>
2025-09-03 23:16:42 -0300

    corrección en importación de moment/locale/es.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import moment from 'moment';
 import 'moment-timezone';
-import 'moment/locale/es';
+import 'moment/locale/es.js';
 
 import initializedPassport from './src/config/passport.config.js';
 import config from './src/config/index.js';
